daa8b632d00780b54dfa8c73f6534f25b803ff18
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-14 23:55:06 +0300

    Wrote NavBar unit test

---
+++ src/__tests__/NavBar.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { NavBar } from "@/components/NavBar";
+import { history } from "@/history";
+import { renderInRouter } from "@/utils/renderInRouter";
+import { Urls } from "@/types/urls";
+
+const setup = () => renderInRouter(<NavBar />);
+
+describe("NavBar", () => {
+  beforeEach(() => {
+    history.push("/");
+    setup();
+  });
+
+  it("should render successfully", () => {
+    const { baseElement } = render(<NavBar />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it("should navigate to browse page when 'Browse Beers' is clicked", () => {
+    const browseButton = screen.getByText("Browse Beers");
+    fireEvent.click(browseButton);
+    console.log(history.location.pathname);
+    expect(history.location.pathname).toBe(Urls.Browse);
+  });
+
+  it("should navigate to / when 'View Favorites' is clicked", () => {
+    const browseButton = screen.getByText("View Favorites");
+    fireEvent.click(browseButton);
+    expect(history.location.pathname).toBe(Urls.Favorites);
+  });
+});
