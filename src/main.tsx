cc422b5e6c5cf8b4c9ca16542a7c77323e1bfc7c
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-14 20:40:06 +0300

    Added index.css with tailwind and imported it

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
