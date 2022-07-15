20c20ad87221f1d0f48a898646acb5185e52110e
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-15 17:10:25 +0300

    Added pages dir and updated App.tsx

---
+++ src/pages/Browse.tsx
@@ -0,0 +1,8 @@
+export function BrowsePage() {
+  return (
+    <>
+      <h1>Welcome to browse page</h1>
+      <p>Beers will be placed here</p>
+    </>
+  );
+}
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,8 @@
+export function FavoritesPage() {
+  return (
+    <>
+      <h1>Welcome to favorites page</h1>
+      <p>Favorite beers will be placed here</p>
+    </>
+  );
+}
