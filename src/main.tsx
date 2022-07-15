59d6f2884bd241ecea05137145fbd491ffb37ea4
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-15 16:51:33 +0300

    Moved index.css to assets folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./assets/index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
