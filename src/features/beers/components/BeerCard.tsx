2b61f8a743a52ac9063849858618acd67c2719bf
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 16:49:01 +0300

    Added modal to BeerCard

--- src/features/beers/components/BeerCard.tsx
+++ src/features/beers/components/BeerCard.tsx
@@ -1,28 +1,57 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { IBeer } from "@/features/beers";
+import { add } from "@/features/favoriteBeers";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { useState } from "react";
+import ReactModal from "react-modal";
+import { Modal } from "./Modal";
 
 interface BeerCardProps {
   beer: IBeer;
 }
 
 export function BeerCard({ beer }: BeerCardProps) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div
-      key={beer.id}
-      className="flex flex-col justify-items-center max-w-xs bg-blue-50 rounded-lg border-4 border-blue-400 shadow-lg aspect-square"
-    >
-      <img src={beer.image_url} alt={beer.name} className="pt-4 mx-auto h-60" />
-      <span className="mt-1 w-full bg-gray-500 p-[1px]" />
-      <div className="p-6">
-        <h2 className="text-lg font-medium leading-6 text-gray-900">
-          {beer.name}
-        </h2>
-      </div>
-      <button
-        type="button"
-        className="self-center py-2 px-3 my-4 text-sm text-white bg-blue-600 rounded border border-transparent shadow-sm hover:bg-blue-700"
+    <>
+      <ReactModal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        className="absolute left-1/2 top-1/4 w-1/2 -translate-x-1/2 md:w-1/3"
+        shouldCloseOnOverlayClick
       >
-        Add To Favorites +
-      </button>
-    </div>
+        <Modal beer={beer} />
+      </ReactModal>
+      <div
+        key={beer.id}
+        className="flex flex-col justify-items-center max-w-xs bg-blue-50 rounded-lg border-4 border-blue-400 shadow-lg aspect-square"
+      >
+        <button
+          type="button"
+          className="flex flex-col"
+          onClick={() => setShowModal(true)}
+        >
+          <img
+            src={beer.image_url}
+            alt={beer.name}
+            className="pt-4 mx-auto h-60"
+          />
+          <span className="mt-1 w-full bg-gray-500 p-[1px]" />
+          <div className="p-6">
+            <h2 className="text-lg font-medium leading-6 text-gray-900">
+              {beer.name}
+            </h2>
+          </div>
+        </button>
+        <button
+          type="button"
+          className="self-center py-2 px-3 my-4 text-sm text-white bg-blue-600 rounded border border-transparent shadow-sm hover:bg-blue-700"
+        >
+          Add To Favorites +
+        </button>
+      </div>
+    </>
   );
 }
