7da82473f1617c507acde815abe0c5e190115178
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 15:19:41 +0300

    Fixed BeerCard test

--- src/features/beers/__tests__/BeerCard.test.tsx
+++ src/features/beers/__tests__/BeerCard.test.tsx
@@ -1,44 +1,50 @@
-import { fireEvent, renderHook, screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { renderWithProviders } from "@/test/renderWithProviders";
 import { getRandomBeer } from "@/test/beerUtils";
 import { setupStore } from "@/context/store";
-import { clear } from "@/features/favoriteBeers";
-import { useAppSelector } from "@/hooks/useAppSelector";
 import { BeerCard } from "@/features/beers";
+import type { PreloadedState } from "@reduxjs/toolkit";
+import type { RootState } from "@/context/store";
 
 const beer = getRandomBeer();
-const store = setupStore();
-const setup = () => renderWithProviders(<BeerCard beer={beer} />, { store });
+const setup = (preloadedState?: PreloadedState<RootState>) => {
+  const store = setupStore();
+  return renderWithProviders(<BeerCard beer={beer} />, {
+    store,
+    preloadedState,
+  });
+};
 
 describe("BeerCard", () => {
-  beforeEach(() => {
-    setup();
-    store.dispatch(clear());
-  });
   it("should render successfully", () => {
     const { baseElement } = setup();
     expect(baseElement).toBeTruthy();
   });
 
   it("should display the beer properly", () => {
-    expect(beer.imageURL).toBeInTheDocument();
+    setup();
+    expect(beer.image_url).toBeInTheDocument();
     expect(beer.name).toBeInTheDocument();
   });
 
   it("should show the modal when a beer is clicked on", () => {
-    const beerModal = screen.getByText("beer.name");
+    setup();
+    const beerModal = screen.getByText(beer.name);
     fireEvent.click(beerModal);
     expect(beer.description).toBeInTheDocument();
-    expect(beer.firstBrewed).toBeInTheDocument();
-    expect(beer.foodPairing).toBeInTheDocument();
+    expect(beer.first_brewed).toBeInTheDocument();
+    expect(beer.food_pairing).toBeInTheDocument();
   });
 
   it("should add a beer to favorites when add to favorites is clicked on", () => {
-    const {
-      result: { current },
-    } = renderHook(() => useAppSelector((s) => s.favoriteBeers));
+    const { store } = setup();
     const addToFavorites = screen.getByText("Add To Favorites");
     fireEvent.click(addToFavorites);
-    expect(current.favoriteBeers.has(beer)).toBeTruthy();
+    const {
+      favoriteBeers: { favoriteBeers },
+    } = store.getState();
+    expect(
+      favoriteBeers.findIndex((b) => b.id === beer.id)
+    ).toBeGreaterThanOrEqual(0);
   });
 });
