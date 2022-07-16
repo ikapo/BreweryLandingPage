50a934776c4978f72f1fdf34b935d77a47d823cc
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 15:38:32 +0300

    Added basic BeerCard info

--- src/features/beers/components/BeerCard.tsx
+++ src/features/beers/components/BeerCard.tsx
@@ -1,9 +1,28 @@
-import { Beer } from "@/features/beers";
+import { IBeer } from "@/features/beers";
 
 interface BeerCardProps {
-  beer: Beer;
+  beer: IBeer;
 }
 
 export function BeerCard({ beer }: BeerCardProps) {
-  return <></>;
+  return (
+    <div
+      key={beer.id}
+      className="flex flex-col justify-items-center max-w-xs bg-blue-50 rounded-lg border-4 border-blue-400 shadow-lg aspect-square"
+    >
+      <img src={beer.image_url} alt={beer.name} className="pt-4 mx-auto h-60" />
+      <span className="mt-1 w-full bg-gray-500 p-[1px]" />
+      <div className="p-6">
+        <h2 className="text-lg font-medium leading-6 text-gray-900">
+          {beer.name}
+        </h2>
+      </div>
+      <button
+        type="button"
+        className="self-center py-2 px-3 my-4 text-sm text-white bg-blue-600 rounded border border-transparent shadow-sm hover:bg-blue-700"
+      >
+        Add To Favorites +
+      </button>
+    </div>
+  );
 }
