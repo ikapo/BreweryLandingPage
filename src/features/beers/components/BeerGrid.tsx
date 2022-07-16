d84f985c20b621a7e706c59fbdf4e70a626bd3fa
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 18:23:41 +0300

    Fixed BeerGrid css

--- src/features/beers/components/BeerGrid.tsx
+++ src/features/beers/components/BeerGrid.tsx
@@ -7,10 +7,12 @@ interface BeerGridProps {
 
 export function BeerGrid({ beers }: BeerGridProps) {
   return (
-    <div className="mx-12 mt-12 space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:mt-16 sm:space-y-0 lg:mx-auto xl:grid-cols-4">
-      {beers.map((beer) => (
-        <BeerCard key={beer.name} beer={beer} />
-      ))}
+    <div className="flex justify-center items-center">
+      <div className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:mt-16 sm:space-y-0 lg:mx-auto xl:grid-cols-3">
+        {beers.map((beer) => (
+          <BeerCard key={beer.name} beer={beer} />
+        ))}
+      </div>
     </div>
   );
 }
