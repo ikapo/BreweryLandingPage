c179102960ae2803711b4af90eb4b74b8e361183
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 21:12:54 +0300

    Fixed BeerGrid overflow

--- src/features/beers/components/BeerGrid.tsx
+++ src/features/beers/components/BeerGrid.tsx
@@ -7,8 +7,8 @@ interface BeerGridProps {
 
 export function BeerGrid({ beers }: BeerGridProps) {
   return (
-    <div className="flex justify-center items-center">
-      <div className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:mt-16 sm:space-y-0 lg:mx-auto xl:grid-cols-3">
+    <div className="flex overflow-scroll no-scrollbar justify-center items-center my-4 max-h-[calc(100vh-10rem)]">
+      <div className="gap-y-4 gap-x-36 space-y-4 sm:grid sm:grid-cols-2 sm:mt-6 sm:space-y-0 lg:mx-auto xl:grid-cols-3">
         {beers.map((beer) => (
           <BeerCard key={beer.name} beer={beer} />
         ))}
