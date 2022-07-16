93ed7c0e781598adeb1bfaa74ed65ae07c3f6ad4
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-16 22:29:45 +0300

    Added state to Favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,4 +1,7 @@
+import { useAppSelector } from "@/hooks/useAppSelector";
+
 export function FavoritesPage() {
+  const favorites = useAppSelector((s) => s.favoriteBeers.favoriteBeers);
   return (
     <>
       <h1>Welcome to favorites page</h1>
