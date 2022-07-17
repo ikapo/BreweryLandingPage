d1bfe6de34f523c76fd1e856d5b9050d7fa8191a
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-17 12:38:00 +0300

    Made BeerRank a dropdown instead of radio

--- src/features/favoriteBeers/components/BeerRank.tsx
+++ src/features/favoriteBeers/components/BeerRank.tsx
@@ -9,8 +9,8 @@ interface BeerRankProps {
 
 export function BeerRank({ beer }: BeerRankProps) {
   const dispatch = useAppDispatch();
-  const setNewRank = (newRank: number) => {
-    const rank = newRank as 1 | 2 | 3 | 4 | 5;
+  const setNewRank = (newRank: string) => {
+    const rank = parseInt(newRank, 10) as 1 | 2 | 3 | 4 | 5;
     dispatch(
       updateRank({
         id: beer.id,
@@ -19,27 +19,23 @@ export function BeerRank({ beer }: BeerRankProps) {
     );
   };
   return (
-    <div className="flex flex-row justify-center items-center p-4 bg-gray-200">
+    <div className="flex flex-row justify-center items-center p-1 bg-gray-200">
       <label className="mr-2 text-base font-medium text-gray-900">Rank:</label>
       <form className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-0">
-        {[1, 2, 3, 4, 5].map((rank) => (
-          <div key={rank} className="flex items-center">
-            <input
-              name="notification-method"
-              id={`${rank}`}
-              type="radio"
-              onChange={() => setNewRank(rank)}
-              checked={rank === beer.rank}
-              className="my-2 mx-1 w-4 h-4 text-gray-600 checked:border-none"
-            />
-            <label
-              htmlFor={`${rank}`}
-              className="block ml-1 text-sm font-medium text-gray-700"
-            >
+        <select
+          id="location"
+          name="location"
+          value={beer.rank}
+          onChange={(rank) => setNewRank(rank.target.value)}
+          className="block py-0.5 pr-10 pl-3 w-full text-base rounded-md border-gray-300 sm:text-sm focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none"
+          defaultValue="Canada"
+        >
+          {[1, 2, 3, 4, 5].map((rank) => (
+            <option className="my-2 mx-1 w-4 h-4 text-gray-600 checked:border-none">
               {rank}
-            </label>
-          </div>
-        ))}
+            </option>
+          ))}
+        </select>
       </form>
     </div>
   );
