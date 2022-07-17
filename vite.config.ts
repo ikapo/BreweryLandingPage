5d2add4e7249ae192661b1661360d96aeec791b9
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-17 21:42:41 +0300

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+    open: false,
+  },
 });
