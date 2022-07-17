515b8bf79d897b4b6f499e1fd0d8c9131de64392
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-17 16:03:57 +0300

    Cleared search on NavBar clicking

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/require-default-props */
+import { clear } from "@/features/search";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
 import { Urls } from "@/types/urls";
 import React from "react";
 import { NavLink } from "react-router-dom";
@@ -8,6 +10,7 @@ interface NavBarProps {
 }
 
 export function NavBar({ children }: NavBarProps) {
+  const dispatch = useAppDispatch();
   return (
     <nav className="px-12 mx-auto bg-gray-800">
       <div className="flex relative justify-between h-16">
@@ -17,11 +20,13 @@ export function NavBar({ children }: NavBarProps) {
         <div className="flex relative inset-0 justify-center items-center mx-28 sm:absolute">
           <NavLink
             className="py-2 px-3 mx-4 text-sm font-medium text-gray-300 rounded-lg hover:text-white hover:bg-gray-700"
+            onClick={() => dispatch(clear())}
             to={Urls.Browse}
           >
             Browse Beers
           </NavLink>
           <NavLink
+            onClick={() => dispatch(clear())}
             className="py-2 px-3 mx-4 text-sm font-medium text-gray-300 rounded-lg hover:text-white hover:bg-gray-700"
             to={Urls.Favorites}
           >
