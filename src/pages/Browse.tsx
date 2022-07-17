c88e8fd6f4c82540ae318f7f8e5c393bd28fdaf0
ikapo <63411550+ikapo@users.noreply.github.com>
2022-07-17 21:00:24 +0300

    Added global error boundary

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -2,7 +2,6 @@ import { useQuery } from "react-query";
 import type { IBeer } from "@/features/beers";
 import { BeerGrid } from "@/components/BeerGrid";
 import { Spinner } from "@/components/Spinner";
-import { isErrorObject } from "@/utils/isErrorObject";
 import { nextPage, PaginatedFooter, previousPage } from "@/features/pagination";
 import { fetchBeers } from "@/utils/fetchBeers";
 import { isValidPage, MAX_PAGE } from "@/config/pagination";
@@ -17,33 +16,16 @@ export function BrowsePage() {
   const dispatch = useAppDispatch();
   const searchStr = useAppSelector((s) => s.search.searchStr);
 
-  const { isLoading, isError, error, data, isFetching } = useQuery(
+  const { isLoading, data, isFetching } = useQuery(
     ["beerPage", page, searchStr],
     () => fetchBeers(page, searchStr),
     {
-      // Caching
-      keepPreviousData: true,
+      retry: 0,
+      useErrorBoundary: true,
+      // eslint-disable-next-line no-console
+      onError: (err) => console.error(err),
     }
   );
-
-  if (isError) {
-    let message = "unknown";
-    if (isErrorObject(error)) {
-      message = error.message;
-    }
-    return (
-      <>
-        <NavBar />
-        <Background />
-        <div className="flex flex-col justify-center h-[calc(100vh-10rem)]">
-          <h2 className="text-8xl text-center text-red-400">
-            An Error occurred:
-          </h2>
-          <h3 className="text-8xl text-center text-red-400">{message}</h3>
-        </div>
-      </>
-    );
-  }
 
   let beers = data as IBeer[];
   // Edge case for getting to max beers
